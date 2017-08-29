0fcb73bff8b2fde164b905f523eecb4e5bf1f11e
rhkanrdl <emrdl7@gmail.com>
2017-08-29 16:07:18 +0900

    완

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 
 $(document).ready(function () {
+
+  $('#iframe .slider').bxSlider({
+    auto: true,
+    controls: false,
+    pager: false,
+    infiniteLoop: true,
+    minSlides: 4,
+    maxSlides: 4
+  });
+
 });
